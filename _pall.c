5b793f67f1d5e9224d5fde4d55f679a4e5d16cda
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-12-17 03:14:46 -0800

    print all values

--- _pall.c
+++ _pall.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
 /**
- * _pall - prints all the values on the stack, starting from the top
+ * pall - prints all the values on the stack, starting from the top
  * @stack: pointer to the stack
  * @line_number: number of lines
  * Return: nothing
  */
-void _pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
