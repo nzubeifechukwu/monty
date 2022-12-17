24148d8d409081b57ac4d8441794bf9af7babf48
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-12-17 03:43:27 -0800

    swap the first two elements on the stack

---
+++ _swap.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to the stack
+ * @line_number: number of lines
+ * Return: nothing
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->next;
+	temp->next = *stack;
+	temp->prev = NULL;
+	(*stack)->next = temp->next;
+	(*stack)->prev = temp;
+	(*stack)->next->prev = *stack;
+	*stack = temp;
+}
